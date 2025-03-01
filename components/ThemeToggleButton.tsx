19c1fd1796a8d055b89cef22a143187afa7fda04
Uriel Campos <urielcampos448@gmail.com>
2025-03-01 11:21:58 -0300

    refactor Header component to use ThemeToggleButton for theme switching

---
+++ components/ThemeToggleButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export default function ThemeToggleButton() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <Button
+      variant={"ghost"}
+      onClick={() => {
+        setTheme(theme === "dark" ? "light" : "dark");
+      }}
+    >
+      {theme === "dark" ? <Sun size={20} /> : <Moon size={20} />}
+    </Button>
+  );
+}
--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import { Button } from "./ui/button";
-import { Moon, Sun } from "lucide-react";
+import ThemeToggleButton from "./ThemeToggleButton";
 
-export default function Header () {
-    const {theme, setTheme} = useTheme();
-    return (
-        <header className="flex justify-between items-center py-4 px-8 sticky top-0 left-0 z-50">
-            <Button variant={"ghost"} onClick={() => {
-                setTheme(theme === "dark" ? "light" : "dark");
-            }}> 
-                {theme === "dark" ? <Sun size={100}/> : <Moon/>}
-            </Button>
-        </header>
-    );
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center py-4 px-8 sticky top-0 left-0 z-50">
+      <ThemeToggleButton />
+    </header>
+  );
 }
