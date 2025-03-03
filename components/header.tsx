a3b1b335f1bbda77f2cb60ae759d50a00491106a
Uriel Campos <urielcampos448@gmail.com>
2025-03-03 10:38:54 -0300

    fix: update Header component styles for improved visual consistency and enhance button cursor behavior

--- components/header.tsx
+++ components/header.tsx
@@ -11,14 +11,14 @@ export default function Header({
   lang: string 
 }) {
   return (
-    <header className="flex justify-between items-center py-4 px-8 sticky top-0 left-0 z-50">
+    <header className="flex justify-between items-center py-4 px-8 sticky top-0 left-0 z-50 from-foreground/20 to-75% bg-gradient-to-b">
       <ThemeToggleButton />
       <Image
         src={image}
         width={125}
         height={100}
         alt="logo"
-        className="mx-auto invert"
+        className="mx-auto drop-shadow-2xl shadow-foreground"
       />
       <LanguageSwitcher currentLang={lang} />
     </header>
